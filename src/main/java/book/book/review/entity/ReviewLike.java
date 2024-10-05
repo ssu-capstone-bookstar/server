8df1b20ae32dbc684dc3a6b3560c38bc21b5f5e8
sunwon <jsw5913@naver.com>
2024-10-05 14:37:29 +0900

    엔티티 init

--- src/main/java/book/book/review/entity/ReviewLike.java
+++ src/main/java/book/book/review/entity/ReviewLike.java
@@ -3,9 +3,11 @@ package book.book.review.entity;
 import book.book.common.BaseTimeEntity;
 import book.book.member.entity.Member;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Table(name = "review_like")
+@Getter
 public class ReviewLike extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/book/book/scrap/entity/Scrap.java
+++ src/main/java/book/book/scrap/entity/Scrap.java
@@ -4,9 +4,11 @@ import book.book.book.entity.Book;
 import book.book.common.BaseTimeEntity;
 import book.book.member.entity.Member;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 @Entity
 @Table(name = "scrap")
+@Getter
 public class Scrap extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
