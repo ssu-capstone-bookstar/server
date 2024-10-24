2fac127586b7673020f2c701f12a27988ec5c2d4
sunwon <jsw5913@naver.com>
2024-10-24 18:14:43 +0900

    chore(Swagger): 맴버 API에 @Tag 붙임

--- src/main/java/book/book/member/api/MemberApi.java
+++ src/main/java/book/book/member/api/MemberApi.java
@@ -4,6 +4,7 @@ import book.book.common.response.ResponseForm;
 import book.book.member.dto.MinimmumMemberProfile;
 import book.book.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/member")
+@Tag(name = "Member", description = "맴버 API")
 public class MemberApi {
 
     private final MemberService memberService;
