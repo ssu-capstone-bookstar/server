a04dda3a55d046466dd427d6c09eed0430ca2deb
sunwon <jsw5913@naver.com>
2024-10-24 15:59:42 +0900

    chore(swagger): swagger에 설명 추가를 위한 어노테이션 추가

--- src/main/java/book/book/search/api/SearchApi.java
+++ src/main/java/book/book/search/api/SearchApi.java
@@ -4,6 +4,8 @@ import book.book.common.response.CursorPageResponse;
 import book.book.common.response.ResponseForm;
 import book.book.search.dto.SearchBookResponse;
 import book.book.search.service.SearchService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +19,15 @@ public class SearchApi {
 
     private final SearchService searchService;
 
+    @Operation(
+            summary = "알라딘 API로 도서 검색"
+    )
     @GetMapping("/books/aladin")
-    public ResponseForm<CursorPageResponse<SearchBookResponse>> getBooksByAladin(@RequestParam String query,
-                                                                                 @RequestParam int start) {
+    public ResponseForm<CursorPageResponse<SearchBookResponse>> getBooksByAladin(
+            @Parameter(name = "query", description = "검색어")
+            String query,
+            @Parameter(name = "start", description = "시작 페이지")
+            @RequestParam int start) {
         return new ResponseForm<>(searchService.getBooksByAladin(query, start));
     }
 
